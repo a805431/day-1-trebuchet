aabb1fc05759e1ff50bb397ded5b03f9d07b774e
Gergana <onyxblack2016@gmail.com>
2023-12-25 16:59:21 +0200

    helper function for regex pattern added

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"os"
 	"regexp"
-	"strconv"
+	"slices"
 )
 
 func main() {
@@ -21,6 +21,18 @@ func main() {
 
 	var fileLines []string
 	result := 0
+	searchValues := map[int]string{
+		1: "one",
+		2: "two",
+		3: "three",
+		4: "four",
+		5: "five",
+		6: "six",
+		7: "seven",
+		8: "eight",
+		9: "nine",
+	}
+	re := regexp.MustCompile(buildRegexPattern(searchValues))
 
 	for fileScanner.Scan() {
 		fileLines = append(fileLines, fileScanner.Text())
@@ -29,18 +41,33 @@ func main() {
 	readFile.Close()
 
 	for _, line := range fileLines {
-		re := regexp.MustCompile("[0-9]")
-		singleLineMatches := re.FindAllString(line, -1)
+		var singleLineMatches []string
+		singleLineMatches = re.FindAllString(line, -1)
+
 		if len(singleLineMatches) > 1 {
-			matchNumber, _ := strconv.Atoi(singleLineMatches[0] + singleLineMatches[len(singleLineMatches)-1])
-			result += matchNumber
+			fmt.Println(singleLineMatches)
 		} else if len(singleLineMatches) == 1 {
-			matchNumber, _ := strconv.Atoi(singleLineMatches[0] + singleLineMatches[0])
-			result += matchNumber
+			fmt.Println(singleLineMatches)
 		} else {
 			fmt.Println("No matches found in this line.")
 		}
 	}
 
 	fmt.Println(result)
+}
+
+func buildRegexPattern(valueMap map[int]string) string {
+	var pattern string
+	var keys []int
+	for k := range valueMap {
+		keys = append(keys, k)
+	}
+	slices.Sort(keys)
+	for _, k := range keys {
+		pattern += valueMap[k]
+		if k != keys[len(keys)-1] {
+			pattern += "|"
+		}
+	}
+	return pattern
 }
